0e8988d9884bcc9c7e4cc81559f1f3b55bd925e7
AchiGel <ar.gelashvili@gmail.com>
2024-03-02 13:35:06 +0400

    add components and styles

---
+++ src/components/ToDoBody/ToDoBody.js
@@ -0,0 +1,24 @@
+import "./toDoBody.css";
+
+function ToDoBody() {
+  return (
+    <main>
+      <section></section>
+      <section>
+        <form>
+          <input type="text" />
+          <button>+</button>
+        </form>
+        <div>
+          <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+          </ul>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default ToDoBody;
